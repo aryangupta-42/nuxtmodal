f1c538c82ebfb334c06c01e0f7ad2ee9683cad13
Aryan Gupta <aryan@Aryans-MacBook-Air.local>
2020-03-17 05:01:31 +0530

    Added closing animation

--- components/modal.vue
+++ components/modal.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="modalContainer">
     <div class="modal" v-bind:style="anim">
-      <Header title="Title goes here" :click="handler" />
+      <Header title="Title goes here" :click="handlerTemp" />
       <div class="line" />
       <Body
         content="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
@@ -24,7 +24,9 @@ export default {
   computed: {
     anim () {
       return {
-        animation: `${this.mod} 0.2s`
+        animation: `${this.mod} 0.2s`,
+        transform: `translateY(${this.para}px)`,
+        opacity: this.op
       }
     }
   },
@@ -35,6 +37,18 @@ export default {
   },
   mounted () {
     this.mod = 'fade'
+    this.para = 0
+    this.op = 1
+  },
+  methods: {
+    handlerTemp () {
+      this.mod = 'fade-out'
+      this.para = 50
+      this.op = 0
+      setTimeout(() => {
+        this.$props.handler()
+      }, 180)
+    }
   }
 }
 
